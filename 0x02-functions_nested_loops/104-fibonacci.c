c6c3a2e02b84848b835f7dbe3ae409458aea8a2a
Linda Gor <gorlinda4@gmail.com>
2023-02-22 09:18:23 -0800

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    unsigned long int a = 1, b = 2, c;
+
+    printf("%lu, %lu", a, b);
+
+    for (i = 2; i < 98; i++)
+    {
+        c = a + b;
+        printf(", %lu", c);
+        a = b;
+        b = c;
+    }
+
+    printf("\n");
+    return 0;
+}
+
